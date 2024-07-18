c3749ead31bedf6809d0dc12f6275300dcbf7f64
MrBarryXu <xubin0530@outlook.com>
2024-07-18 14:16:55 +0800

    更新配置
    更新配置

--- metadata/index.ts
+++ metadata/index.ts
@@ -1,17 +1,17 @@
 /** 文本 */
-export const siteName = 'Nólëbase'
-export const siteShortName = 'Nólëbase'
-export const siteDescription = '记录回忆，知识和畅想的地方'
+export const siteName = 'NasKlog'
+export const siteShortName = 'NasKlog'
+export const siteDescription = '撰写技术文章、知识和畅聊的地方'
 
 /** 文档所在目录 */
 export const include = ['笔记', '生活']
 
 /** Repo */
-export const githubRepoLink = 'https://github.com/nolebase/nolebase'
+export const githubRepoLink = 'https://www.nasklog.com/'
 /** Discord */
-export const discordLink = 'https://discord.gg/XuNFDcDZGj'
+export const discordLink = 'https://www.nasklog.com/'
 
 /** 无协议前缀域名 */
-export const plainTargetDomain = 'nolebase.ayaka.io'
+export const plainTargetDomain = 'www.nasklog.com'
 /** 完整域名 */
 export const targetDomain = `https://${plainTargetDomain}`
